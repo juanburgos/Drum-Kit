663f0a36b7e78e711d1dde6e4e7a508f2340f322
Juan Burgos <33945962+juanburgos@users.noreply.github.com>
2019-03-13 12:50:49 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
     makeSound(buttonInnerHTML);
 
+    buttonAnimation(buttonInnerHTML);
+
   });
 
 }
@@ -17,6 +19,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 document.addEventListener("keydown", function(event) {
 
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 
 });
 
@@ -62,4 +66,14 @@ function makeSound(key) {
       console.log(buttonInnerHTML);
 
   }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
